3e68971c79d99dbc09596f7d385606bb46d324d7
Sergio Vilar <sergiovilar.r@gmail.com>
2015-08-16 01:10:05 -0300

    Removed unused contants

--- src/Hero.php
+++ src/Hero.php
@@ -5,7 +5,7 @@ use Hero\Core\ModelLoader;
 
 class Hero {
 
-  private static $app;
+  public static $app;
 
   public function __construct() {
 
@@ -13,17 +13,11 @@ class Hero {
     if(!defined('ALTER')) define('ALTER', __DIR__ . "/..");
     if(!defined('ALTER_VENDOR')) define('ALTER_VENDOR', ALTER . "/..");
 
-    if(!defined('ASSETS_PATH')) define('ASSETS_PATH', get_bloginfo('template_url'));
     if(!defined('APPLICATION_PATH')) define('APPLICATION_PATH', get_template_directory());
 
     $path = explode('wp-content', realpath(ALTER_VENDOR . "/meta-box/"));
     if(!defined('RWMB_URL')) define('RWMB_URL', get_site_url().'/wp-content'.$path[1].'/');
     if(!defined('RWMB_DIR')) define('RWMB_DIR', ALTER_VENDOR . "/meta-box/" );
-
-    // Assets constants
-    if(!defined('ALTER_IMG')) define('ALTER_IMG', ASSETS_PATH . "/img");
-    if(!defined('ALTER_CSS')) define('ALTER_CSS', ASSETS_PATH . "/css");
-    if(!defined('ALTER_JS')) define('ALTER_JS', ASSETS_PATH . "/js");
 
     // ---- Import framework dependencies in the right order (Composer sucks for that!)
     require_once ALTER_VENDOR . "/php-form-generator/fg/load.php";
